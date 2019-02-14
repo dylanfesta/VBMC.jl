30402ec4ed1172c717dd919f839e626e5567e6d3
dylanfesta <darksnail@gmail.com>
2019-02-13 19:10:23 -0500

    Divide et impera! Data structure and modular functions approach

--- src/utils/slicesampler.jl
+++ src/utils/slicesampler.jl
@@ -1,8 +1,14 @@
 
 
+# function to deal with parameter transformations
+
+
 # auxiliary structure for a more modular approach
 struct SliceSamplerPars
-  x
+  samples::Matrix{Float64}
+  xx
+  xx_sum
+  xx_sqsum
   xprime
   xl
   xr
@@ -10,60 +16,91 @@ struct SliceSamplerPars
   UB
   LB_out
   UB_out
-  logpdf_bound
-  logprior
+  widths
+  basewidths
+  nthin::Integer
+  nburn::Integer
+  state::SliceSamplerState
+  logpdf::Function
+  logprior::Function
+  dostepout::Bool
+  isadaptive::Bool
   # constructor
   function SliceSamplerPars(logpdf::Function ,
     x0::AbstractVector{Float64} , nsampl::Integer ;
     logprior::Union{Function,Nothing} = nothing ,
     thinning::Integer = 1 ,
-    burning::Union{Integer,Nothing} = nothing,
+    burnin::Union{Integer,Nothing} = nothing,
     step_out = false, display=true,
-    widths = nothing ,
-    boundaries=(-Inf,Inf),
+    widths::Union{Nothing,AbstractVector{Float64},Float64} = nothing ,
+    boundaries=(-Inf,+Inf),
     dostepout = false ,
-    isadaptive = true )
+    isadaptive = true)
+  D = length(x0)
+  function tovec(x::Real)::Vector{Float64}
+    fill(x,D)
+  end
+  function tovec(x::Vector{T} where T<:Real)::Vector{Float64}
+    @assert length(x) == D  "something wrong in the vector size"
+    return x
+  end
+  xx,xprime,xl,xr = [ copy(x0) for _ in 1:4]
+  xx_sum, xx_sqsum = zero(x0), zero(x0)
+  LB, UB  = [ tovec(_x) for _x in boundaries ]
+  # basewidths used only for adaptation
+  basewidths = something(widths , zero(x))
 
-  x,xprime,xl,xr = [ copy(x0) for _ in 1:4]
-  LB, UB  = [ tovec(x,D) for x in boundaries]
-  widths = let _w =  something(widths , @. (UB - LB) / 2 )
-     tovec(_w,D) end
-  widths[isinf.(widths)] .= 10.0
-  widths[LB .== UB] .= 1.0   # WIDTHS is irrelevant when LB == UB, set to 1
-  LB_out = @. LB-eps(LB) # this is NaN for UB,LB = Inf
-  UB_out = @. UB+eps(UB)
-
-  basewidths = widths
-  doprior = !isnothing(logprior)
-
-end
-
-#=
-function slicesamplebnd(logpdf::Function ,
-  x0::AbstractVector{Float64} , nsampl::Integer ;
-  logprior::Union{Function,Nothing} = nothing ,
-  thinning::Integer = 1 ,
-  burning::Union{Integer,Nothing} = nothing,
-  step_out = false, display=true,
-  widths = nothing ,
-  boundaries=(-Inf,Inf),
-  dostepout = false ,
-  isadaptive = true )
-=#
-
-function get_logprior(p)
-  if isnothing(logprior)
-    0.0
-  else
-    logprior(p.x)
+  widths = if !isnothing(widths)
+              tovec(widths)
+           else
+             deltaB =  @. (UB - LB) / 2
+             map(deltaB) do d
+               isfinite(d) ? d : 10.0
+             end
+          end
+  LB_out = map(x-> isfinite(x) ? x-eps(x) : x , LB)
+  UB_out = map(x-> isfinite(x) ? x+eps(x) : x , UB)
+  function _logprior(x::Vector{Float64})::Float64
+    if isnothing(logprior)
+      return 0.0
+    else
+      logprior(x)
+    end
+  end
+  samples = Matrix{Float64}(undef,nsampl, D)
+  nthin = floor(thinning)
+  burn =  something(burnin, div(nsampl,3))
+  # now check all conditions
+  @assert  all(UB .>= LB)
+       "All upper bounds UB need to be equal or greater than lower bounds LB."
+  @assert all(basewidths .>= 0 .&  isfinite.(basewidths))
+           "The vector widths need to be all positive real numbers."
+  @assert all(widths .> 0 .&  isfinite.(widths))
+          "The vector widths need to be all positive real numbers."
+  @assert all(x0 .>= LB) && all(x0 .<= UB)
+         "The initial starting point X0 is outside the bounds."
+  @assert isfinite(logpdf(x0))
+    "The initial starting point x0 needs to evaluate to a real number (not Inf or NaN)."
+  @assert thin > 0
+        "The thinning factor OPTIONS.Thin needs to be a positive integer."
+  @assert burn >= 0
+         "The burn-in samples burnin needs to be a non-negative integer."
+  if burn == 0 && isadaptive
+    @error(" (non critical) Adaptation is ON but burnin option is set to 0!"*
+              "For adaptive width set burnin > 0.")
+  end
+  new(xx,xx_sum,xx_sqsum,xprime,xl,xr,LB,UB,LB_out,UB_out,widths,basewidths,
+       nthin, nburn,logpdf,_logprior, dostepout , isadaptive)
   end
 end
 
-
-function __logpdf_check_bound(p::SliceSamplerPars)
-  logpri = get_logprior(p)
-  fval = p.get_logPx(p)
-  if  any( (p.x .< p.LB) .| (p.x .> p.UB)  ) || !isfinite(logpri)
+# auxiliary function
+# check results and bounds of the logpdf function
+# the logpdf should return a scalar, and take a vector as input
+function logpdf_bounds(x::AbstractVector{Float64},p::SliceSamplerPars)::Float64
+  logpri = p.logprior(x)
+  fval = p.logpdf(x)
+  if  any( ( x .< p.LB) .| ( x .> p.UB)  ) || !isfinite(logpri)
      return -Inf
   elseif isnan(fval)
     @warn "Target density function returned NaN. Trying to continue."
@@ -74,28 +111,16 @@ end
 
 
 # auxiliary function
-# check results and bounds of the logpdf function
-# the logpdf should return a scalar, and take a vector as input
-function _logpdf_check_bound( LB::Vector{Float64},
-  UB::Vector{Float64}, x::Vector{Float64},
-  fval::Float64, val_logprior::Float64)
-  #checking bounds here
-  if  any( (x.< LB) .| (x.> UB)  ) || !isfinite(val_logprior)
-     return -Inf
-  elseif isnan(fval)
-    @warn "Target density function returned NaN. Trying to continue."
-    return -Inf
-  end
-  fval + val_logprior
-end
-
-# auxiliary function
 # defines new left and righ boundaries
-# over one dimension. Using x0 and width
-function _adjust_bounds(width,LBout,UBout,x0)
+# updating x_l and x_r internally
+# over dimension d. Using x as starting point
+# and taking width as reference
+function _update_bounds!(d,x_start,p::SliceSamplerPars)
   rnd = rand()
-  l = x0 - rnd*width
-  r = x0 + (1. - rnd)*width
+  wdd = width[d]
+  l = x_start - rnd*wdd
+  r = x_start + (1. - rnd)*wdd
+  LBout,UBout = p.LB_out[d], p.UB_out[d]
   if isfinite(LBout) && l < LBout
     delta = LBout - l
     l += delta
@@ -106,69 +131,98 @@ function _adjust_bounds(width,LBout,UBout,x0)
     r -= delta
     l -= delta
   end
-  max(l,LBout) , min(r,UBout)
+  # maybe this is an overkill
+  p.x_l[d] = max(l,LBout)
+  p.x_r[d] = min(r,UBout)
+  nothing
 end
-
-
-
 
 
 # auxiliary function to sample point in the slice
 # shrinks the interval and updates the boundaries when outside of the volume
 # the x(prime) vector is updated
-function _do_shrink(nshrink::Integer,
-    idx::Integer,
-    targ_lpdf::Float64,
-    lpdf::Function, x::AbstractVector{Float64}, xref::Float64,
-    xl::Float64,xr::Float64)
+function _shrink(d,nshrink,current_logPx::Float64,p::SliceSamplerPars)
   nshrink += 1
-  x[idx] = rand()*(xr - xl) + xl
-  # the sample is inside , return
-  logPx::Float64 = lpdf(x)
-  if  logPx < targ_lpdf
-    return (logPx,nshrink,xl,xr)
-  end
-  # the sample is outside,  shrink the boundaries, repeat
-  if x[idx] > xref
-      xr = x[idx]
-  elseif x[idx] < xref
-      xl = x[idx]
-  else # error when xl and xr collapse unto each other
-      error("Shrunk to current position and proposal still not acceptable.")
+  x_try = rand()*(p.x_r[d] - p.x_l[d]) + p.x_l[d]
+  p.xprime[d] = x_try
+  logPx::Float64 = logpdf_bounds(p.xprime,p)
+  isgood = new_logPx < current_logPx
+  # the sample is outside,  shrink the boundaries
+  if !isgood
+    if x_try > xref
+        x_r[d] .= x_try
+    elseif x_try < xref
+        x_l[d] .= x_try
+    else # error when xl and xr collapse unto each other
+      error("Boundaries shrunk to current position and proposal still not acceptable.")
       # Current position: $xx  Log f: (new value) $log_Px)
       # , (target value)  $log_uprime" )
+    end
   end
-  _do_shrink(nshrink,idx,targ_lpdf,lpdf,x, xref, xl,xr)
+  return (new_logPx,nshrink,isgood)
 end
 
 # auxiliary function, updates left and right boundaries in place
-function _do_step_out(targ_lpdf::Float64,lpdf::Function, stepsize::Float64,
-      xl::Vector{Float64},xr::Vector{Float64})
+function _do_step_out(d,current_logPx::Float64, p::SliceSamplerPars)
   steps = 0
-  while lpdf(x_l) > targ_lpdf
-      x_l[dd] .-= stepsize
-      steps += 1
-  end
-  while lpdf(x_r) > targ_lpdf
-      x_r[dd] .+= stepsize
-      steps += 1
+  if p.dostepout
+    while logpdf_bounds(x_l,p) > current_logPx
+        x_l[d] .-= stepsize
+        steps += 1
+    end
+    while logpdf_bounds(x_r,p) > current_logPx
+        x_r[d] .+= stepsize
+        steps += 1
+    end
   end
   return steps
 end
 
-# auxiliary function , with adaptation and during burn-in
-# proposes a new width given the old one
-function _adapt_width(delta_bound::Float64,
-        nshrink::Integer,oldwidth::Float64)
+# auxiliary function
+# performs width adaptation during burnin
+# does nothing for nshrink = 2 or 3
+function _adapt_width_burning!(ii,d,nshrink::Integer,p::SliceSamplerPars)
+  # do nothing without burning conditions
+  if (ii > p.nburn) || (! p.isadaptive)
+    return nothing
+  end
+  delta_bound = p.UB[d] - p.LB[d]
+  w_pre = p.widths[d]
   if nshrink > 3
     delta_eps = isfinite(delta_bound) ? eps(delta_bound) : eps()
-    return max(oldwidth/1.1,delta_eps)
+    p.widths[d] .= max(w_pre/1.1,delta_eps)
   elseif nshrink < 2
-    return min(oldwidth, delta_bound)
-  else
-    return oldwidth
+    p.widths[d] .=  min(w_pre, delta_bound)
+  end
+  nothing
+end
+
+
+# Store summary statistics starting half-way into burn-in
+function _update_summary!(ii,p::SliceSamplerPars)
+  if ii <= p.burn && ii > div(p.nburn,2)
+    p.xx_sum .+=  xx
+    p.xx_sqsum .+=  xx.^2
+  end
+  nothing
+end
+
+# auxuliary function, performs width adaptation at the end of the burn-in
+# procedure
+function _adapt_width_burn_end!(ii,p::SliceSamplerPars)
+  if (ii != p.nburn) || (! p.isadaptive)
+    return nothing
+  end
+  burnstored = div(p.nburn,2)
+  map!(p.widths, zip(p.xx_sqsum,p.xx_sum,
+                   p.UB_out,p.LB_out,p.basewidths)) do (xsq,xs,ub,lb,wbase)
+    neww = min( 5.0sqrt(xsq/burnstored - (xs/burnstored)^2) , ub-lb)
+    # Max between new widths and geometric mean with user-supplied
+    # widths (i.e. bias towards keeping larger widths)
+    max(neww,sqrt(neww*wbase))
   end
 end
+
 
 function slicesamplebnd(logpdf::Function ,
   x0::AbstractVector{Float64} , nsampl::Integer ;
